1349e3906649857a9b206a6b6244c0b5ae93035d
Baraev Sergey <Baraev.sergey@mail.ru>
2021-03-31 21:30:22 +0500

    Добил номера name-ов + скрыл пароли

--- django_pm/mainapp/forms.py
+++ django_pm/mainapp/forms.py
@@ -3,15 +3,15 @@ from django import forms
 class InputForm(forms.Form):
     name = forms.CharField(label="Ваш сайт", max_length=50)
     login = forms.CharField(label="Ваш логин", max_length=50)
-    password = forms.CharField(label="Ваш пароль", min_length=8, max_length=50)
+    password = forms.CharField(label="Ваш пароль", min_length=8, max_length=50,widget=forms.PasswordInput())
 
 class LoginForm(forms.Form):
     login = forms.CharField(label="Введите логин", max_length=50)
-    password = forms.CharField(label="Введите пароль", max_length=50)
+    password = forms.CharField(label="Введите пароль", max_length=50, widget=forms.PasswordInput())
 
 class RegisterForm(forms.Form):
     login = forms.CharField(label="Введите логин", max_length=50)
-    password = forms.CharField(label="Введите пароль", max_length=50, min_length=8)
-    confirm_pass = forms.CharField(label="Подтвердите пароль", max_length=50, min_length=8)
+    password = forms.CharField(label="Введите пароль", max_length=50, min_length=8, widget=forms.PasswordInput())
+    confirm_pass = forms.CharField(label="Подтвердите пароль", max_length=50, min_length=8, widget=forms.PasswordInput())
 
 
